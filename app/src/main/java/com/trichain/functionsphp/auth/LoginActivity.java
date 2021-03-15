3a4b7256af93ce60f29a6ed2ac4c75d3c31871e4
Yoosin Paddy <yoosinfrancis@gmail.com>
2021-03-15 09:04:49 +0300

    LoginActivity

--- app/src/main/java/com/trichain/functionsphp/auth/LoginActivity.java
+++ app/src/main/java/com/trichain/functionsphp/auth/LoginActivity.java
@@ -1,16 +1,39 @@
 package com.trichain.functionsphp.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.trichain.functionsphp.R;
+import com.trichain.functionsphp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-
+ActivityLoginBinding b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        b= DataBindingUtil.setContentView(this,R.layout.activity_login);
+    }
+
+    public void validate(View view){
+        if (b.emailED.getText().toString().isEmpty()){
+            b.emailED.setError("Please fill this");
+            b.emailED.requestFocus();
+        }else if (b.passwordED.getText().toString().isEmpty()){
+            b.passwordED.setError("Please fill this");
+            b.passwordED.requestFocus();
+        }else if (!b.emailED.getText().toString().contains("@")){
+            b.emailED.setError("Please enter a valid email");
+            b.emailED.requestFocus();
+        }else{
+            networkLogin();
+        }
+    }
+
+    private void networkLogin() {
+        b.progressCircular.show();
+        b.progressCircular.hide();
     }
 }
